5e507b351dca940793187f7ab113b82699c9ad55
Alexander Mantzaris <al062959@cosd4vgzc2.net.ucf.edu>
2017-01-05 18:04:43 -0500

    added the ability to put a tick vector for the x and y axis

--- mesh_make.py
+++ mesh_make.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
 """
-
 #the rows of the mat correspond to the X-axis
 #the cols to the Y-axis
 #the matrix entries to the Z-axis values
 #the first index for the mat is the top left
+#x_pnts and y_pnts are optional positions of these coordinates
 """
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 
 #produce a mesh from the mat
-def im2mesh( mat):
+def im2mesh(mat,x_pnts=-1,y_pnts=-1):
     X_num = mat.shape[0]
     Y_num = mat.shape[1]
-    #each X1 row is a repeat, as many rows as Y values
-    X1 = np.tile(np.array(range(X_num)),(Y_num,1))
-    #each Y1 column is constant
-    Y1 = np.transpose(np.tile(np.array(range(Y_num)),(X_num,1)))
-    #the structure of the needed points
-    Z1 = np.transpose(mat)#np.random.rand(Y_num,X_num)    
+    print(x_pnts)
+    if(x_pnts == -1):
+        x_pnts = np.array(range(X_num))
+    if(y_pnts == -1):
+        y_pnts = np.array(range(Y_num))
+    print(x_pnts)
+    X1 = np.tile(x_pnts,(Y_num,1))    
+    Y1 = np.transpose(np.tile(y_pnts,(X_num,1)))    
+    Z1 = np.transpose(mat)#np.random.rand(Y_num,X_num)
+    
     return X1,Y1,Z1
 
 
-def im2wireframe(mat):
+def im2wireframe(mat,x_pnts=-1,y_pnts=-1):
+    print(x_pnts)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    X1,Y1,Z1 = im2mesh(mat)
+    X1,Y1,Z1 = im2mesh(mat,x_pnts,y_pnts)
     ax.plot_wireframe(X1, Y1, Z1, rstride=10, cstride=10)
     plt.show()
