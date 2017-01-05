822b3a5daa5155b25e0f435b065243dbc24aef2e
Alexander Mantzaris <al062959@cosd4vgzc2.net.ucf.edu>
2017-01-05 18:50:36 -0500

    pulling since I updated the README file

--- mesh_make.py
+++ mesh_make.py
@@ -13,24 +13,20 @@ import numpy as np
 #produce a mesh from the mat
 def im2mesh(mat,x_pnts=-1,y_pnts=-1):
     X_num = mat.shape[0]
-    Y_num = mat.shape[1]
-    print(x_pnts)
-    if(x_pnts == -1):
+    Y_num = mat.shape[1]       
+    if(isinstance(x_pnts,int)):
         x_pnts = np.array(range(X_num))
-    if(y_pnts == -1):
-        y_pnts = np.array(range(Y_num))
-    print(x_pnts)
+    if(isinstance(y_pnts,int)):
+        y_pnts = np.array(range(Y_num))    
     X1 = np.tile(x_pnts,(Y_num,1))    
     Y1 = np.transpose(np.tile(y_pnts,(X_num,1)))    
-    Z1 = np.transpose(mat)#np.random.rand(Y_num,X_num)
-    
+    Z1 = np.transpose(mat)    
     return X1,Y1,Z1
 
 
 def im2wireframe(mat,x_pnts=-1,y_pnts=-1):
-    print(x_pnts)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
     X1,Y1,Z1 = im2mesh(mat,x_pnts,y_pnts)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')    
     ax.plot_wireframe(X1, Y1, Z1, rstride=10, cstride=10)
     plt.show()
