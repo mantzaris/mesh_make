81d4f7cf53dc455389c713af59a8a1f74965514d
Alexander Mantzaris <al062959@cosd4vgzc2.net.ucf.edu>
2017-01-05 17:03:52 -0500

    finalizing the wireframe im

--- mesh_make.py
+++ mesh_make.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-EX. from .py import fn
+
 #the rows of the mat correspond to the X-axis
 #the cols to the Y-axis
 #the matrix entries to the Z-axis values
@@ -12,10 +12,6 @@ import numpy as np
 
 #produce a mesh from the mat
 def im2mesh( mat):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    X, Y, Z = axes3d.get_test_data(0.05)
-
     X_num = mat.shape[0]
     Y_num = mat.shape[1]
     #each X1 row is a repeat, as many rows as Y values
@@ -23,10 +19,13 @@ def im2mesh( mat):
     #each Y1 column is constant
     Y1 = np.transpose(np.tile(np.array(range(Y_num)),(X_num,1)))
     #the structure of the needed points
-    Z1 = np.transpose(mat)#np.random.rand(Y_num,X_num)
-    print(X1)
-    print(Y1)
-    print(Z1)
+    Z1 = np.transpose(mat)#np.random.rand(Y_num,X_num)    
+    return X1,Y1,Z1
+
+
+def im2wireframe(mat):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X1,Y1,Z1 = im2mesh(mat)
     ax.plot_wireframe(X1, Y1, Z1, rstride=10, cstride=10)
     plt.show()
-    #return X1,Y1,Z1
